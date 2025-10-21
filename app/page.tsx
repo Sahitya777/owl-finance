22f280b9de33a66bc16a6e85e020ced7bfb42119
Sahitya Nijhawan <sahityanijhawan@gmail.com>
2025-10-22 03:00:51 +0530

    BUG_FIX fixed build errors [DONE]

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import { Hero } from "@/components/hero"
 import { Features } from "@/components/features"
 import { HowItWorks } from "@/components/how-it-works"
 import { Footer } from "@/components/footer"
-import { CTA } from "@/components/cta"
 
 export default function Home() {
   return (
